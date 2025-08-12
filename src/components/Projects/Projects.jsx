c87c0633ad9392d5380a1b72cea8f34cc3b0deea
MdNobinfd <mdnobin.fdr@gmail.com>
2025-08-12 19:58:28 +0600

    updated project componets

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -66,6 +66,7 @@ const Projects = () => {
     >
       <div className="absolute inset-0 bg-[#04081A]" />
 
+      {/* Grid background */}
       <div className="absolute inset-0 bg-[linear-gradient(rgba(50,50,70,0.15)_1px,transparent_1px),linear-gradient(90deg,rgba(50,50,70,0.15)_1px,transparent_1px)] bg-[size:40px_40px] [mask-image:radial-gradient(ellipse_80%_80%_at_50%_50%,#000_70%,transparent_100%)]" />
 
       {/* Animated particles */}
