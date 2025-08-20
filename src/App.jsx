2bdfc482b0f5f1c1133fc3fe4d7556b314494143
MdNobinfd <mdnobin.fdr@gmail.com>
2025-08-20 19:25:40 +0600

    preloader added and design  loader components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import Header from './components/Navbar/Navbar';
 import Hero from './components/Hero/HeroSeciton';
 import SkillSecion from './components/skill/SkillSecion';
@@ -7,19 +8,44 @@ import Footer from './components/Footer/Footer';
 import Education from './components/education/Education';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
-// import './assets/css/index.css'
+import Stairs from './components/ui/Stairs';
 
 const App = () => {
+  const [showIntro, setShowIntro] = useState(true);
+
+  useEffect(() => {
+    // 2 সেকেন্ড পরে main content আসবে
+    const timer = setTimeout(() => {
+      setShowIntro(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div>
-      <Header />
-      <Hero />
-      <About />
-      <SkillSecion />
-      <Education />
-      <Projects />
-      <Contact />
-      <Footer />
+      <AnimatePresence mode="wait">
+        {showIntro && (
+          <motion.div>
+
+                       <div className="h-screen w-screen fixed top-0 right-0 left-0 pointer-events-none z-40 flex">
+                        <Stairs></Stairs>
+                    </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+
+      {!showIntro && (
+        <>
+          <Header />
+          <Hero />
+          <About />
+          <SkillSecion />
+          <Education />
+          <Projects />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
